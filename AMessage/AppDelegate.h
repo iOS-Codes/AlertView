9663c02072a1cf3827c0d01a233676ab26707e55
genomatrix <Arjun@arjun.local>
2016-09-19 16:05:39 +0530

    Initial Commit

---
+++ AMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AMessage
+//
+//  Created by Arjun Hanswal on 9/19/16.
+//  Copyright © 2016 Com. PhoneGap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AMessage
+//
+//  Created by Arjun Hanswal on 9/19/16.
+//  Copyright © 2016 Com. PhoneGap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
